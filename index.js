84dfa7304ea6fab2e9f80acab9f91f9631b9b13a
Tatyana404 <kot.kate2020@gmail.com>
2020-12-26 16:14:08 +0200

    Add Task 2 multiplicity check

--- index.js
+++ index.js
@@ -13,3 +13,22 @@ const isAdult = function (value) {
   return false;
 };
 alert(isAdult(userInput));
+
+// Task 2
+
+const userInput1 = +prompt("Введите первое число:");
+const userInput2 = +prompt("Введите второе число:");
+const checkMultiplicity = function (value1, value2) {
+  if (isNaN(value1 - value2)) {
+    return "Вы ввели не число!";
+  }
+
+  if (value1 === 0) {
+    return 0;
+  }
+  if (value2 <= 0) {
+    return "На 0 делить нельзя!";
+  }
+  return value1 % value2 === 0;
+};
+alert(checkMultiplicity(userInput1, userInput2));
