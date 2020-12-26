72ecdec135958b73b93cc7a95a598e5e481cecf5
Tatyana404 <kot.kate2020@gmail.com>
2020-12-26 16:12:34 +0200

    Add Task 1 age check

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 "use strict";
+
+// Task 1
+
+const userInput = +prompt("Введите ваш возраст:");
+const isAdult = function (value) {
+  if (isNaN(value)) {
+    return "Вы ввели не число";
+  }
+  if (value >= 18) {
+    return true;
+  }
+  return false;
+};
+alert(isAdult(userInput));
